4c308cb6db7d8fea70ceae98d2edec85202dd593
uscl-tech <uscl.bilvalabs@gmail.com>
2025-03-20 16:00:38 +0530

    bool

---
+++ basics/bool.rs
@@ -0,0 +1,14 @@
+
+fn main(){
+    let a=false;
+    let b=true;
+    if a {
+        print!("\n\t are you kid \n");
+    }else{
+        print!("\n\t you are adult \n");
+    }
+    if a && b {
+        print!("\n\t You are aliean \n");
+    }
+}
+
